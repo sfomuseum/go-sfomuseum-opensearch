e02883c62f90125a1a6c3d6a940c9eaccabbc9a1
sfomuseumbot <sfomuseumbot@localhost>
2023-11-22 17:13:41 -0800

    index name and description

--- document/embeddings.go
+++ document/embeddings.go
@@ -3,6 +3,7 @@ package document
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
@@ -15,13 +16,27 @@ func SFOMuseumPrepareEmbeddingsDocumentFunc() es_document.PrepareDocumentFunc {
 
 		new_body := []byte(`{}`)
 
-		rsp := gjson.GetBytes(body, "sfomuseum:description")
-
-		if !rsp.Exists() {
-			return nil, nil
+		to_consider := []string{
+			"wof:name",
+			"sfomuseum:description",
 		}
 
-		new_body, err := sjson.SetBytes(new_body, "sfomuseum:description", []byte(rsp.String()))
+		to_index := make([]string, 0)
+
+		for _, path := range to_consider {
+
+			rsp := gjson.GetBytes(body, path)
+
+			if !rsp.Exists() {
+				continue
+			}
+
+			to_index = append(to_index, rsp.String())
+		}
+
+		str_body := strings.Join(to_index, " ")
+
+		new_body, err := sjson.SetBytes(new_body, "sfomuseum:description", []byte(str_body))
 
 		if err != nil {
 			return nil, fmt.Errorf("Failed to assign sfomuseum:description, %w", err)
